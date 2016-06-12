74ceb1fd03d5aa9e921ae35f12f44d41fbe45c00
Hofbauer Benoit <benoit.hofbauer@hermes-ecs.com>
2016-06-12 23:40:22 +0200

    assignement1

---
+++ Exploratory_data_analysis/plot1.R
@@ -0,0 +1,39 @@
+
+#dowload Data
+dataDir <- "./data/"
+if(!dir.exists(dataDir))
+{
+  dir.create(dataDir)
+  url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  zipFile <-paste(dataDir,"power_consumption.zip")
+  zip <- download.file(url, zipFile)
+  unzip(zipFile,exdir = dataDir)
+}
+
+fileHousePowerConsumption <- paste(dataDir, "household_power_consumption.txt", sep="")
+
+#load file in R
+HousePowerConsumption <- read.table( fileHousePowerConsumption, header = TRUE , sep=";",na.strings = "?")
+
+#SubSet
+SubSet <-HousePowerConsumption$Date == "1/2/2007" | HousePowerConsumption$Date == "2/2/2007"
+HousePowerConsumptionSubSet <- HousePowerConsumption[SubSet,]
+
+#use R date
+HousePowerConsumptionSubSet$DateR <-strptime (paste(HousePowerConsumptionSubSet$Date, HousePowerConsumptionSubSet$Time), format = "%d/%m/%Y %T")
+
+par(mfrow = c(1, 1))
+#plot histogramme
+hist(HousePowerConsumptionSubSet$Global_active_power,main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", 
+     ylab = "Frequency", 
+     col = "red")
+
+
+
+
+# copy plot to png file
+dev.copy(png, file = "plot1.png")
+
+# close connection to png device
+dev.off()
---
+++ Exploratory_data_analysis/plot3.R
@@ -0,0 +1,43 @@
+
+#dowload Data
+dataDir <- "./data/"
+if(!dir.exists(dataDir))
+{
+  dir.create(dataDir)
+  url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  zipFile <-paste(dataDir,"power_consumption.zip")
+  zip <- download.file(url, zipFile)
+  unzip(zipFile,exdir = dataDir)
+}
+
+fileHousePowerConsumption <- paste(dataDir, "household_power_consumption.txt", sep="")
+
+#load file in R
+HousePowerConsumption <- read.table( fileHousePowerConsumption, header = TRUE , sep=";",na.strings = "?")
+
+#SubSet
+SubSet <-HousePowerConsumption$Date == "1/2/2007" | HousePowerConsumption$Date == "2/2/2007"
+HousePowerConsumptionSubSet <- HousePowerConsumption[SubSet,]
+
+#use R date
+HousePowerConsumptionSubSet$DateR <-strptime (paste(HousePowerConsumptionSubSet$Date, HousePowerConsumptionSubSet$Time), format = "%d/%m/%Y %T")
+
+par(mfrow = c(1, 1))
+with(   HousePowerConsumptionSubSet, {
+  plot( DateR, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+  lines(DateR, Sub_metering_2, col = "red")
+  lines(DateR, Sub_metering_3, col = "blue")
+  legend("topright", col = c("black", "red", "blue"), cex = 0.7, lty = 1,
+         legend = c("Sub_metering_1             ", 
+                    "Sub_metering_2             ",
+                    "Sub_metering_3             "))
+  
+})
+
+
+
+# copy plot to png file
+dev.copy(png, file = "plot3.png")
+
+# close connection to png device
+dev.off()
